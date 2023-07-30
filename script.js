00c42b68cce83a626e3550cf07e37283e6a6b51c
Ishwaran <39009926+IshwaranRudhara@users.noreply.github.com>
2023-07-31 01:41:23 +0530

    crushed glitches

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector("#nav-list");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navList.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+  })
+);
+
+const toggle = document.getElementById("toggle");
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
